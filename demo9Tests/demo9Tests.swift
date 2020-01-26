d7c78604942ab12005e72de4b19cfb6279fb10cd
chen <chen@gmsil.com>
2020-01-26 15:45:21 +0900

    Initial Commit

---
+++ demo9Tests/demo9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  demo9Tests.swift
+//  demo9Tests
+//
+//  Created by yw c on 2020/01/26.
+//  Copyright © 2020 yw c. All rights reserved.
+//
+
+import XCTest
+@testable import demo9
+
+class demo9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
